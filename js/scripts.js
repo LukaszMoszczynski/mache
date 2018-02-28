76418d5378ecab6b75b757c1dd92700565c6cc96
Łukasz Moszczyński <moszczynski.luk@gmail.com>
2018-02-28 11:47:10 +0100

    v1.0

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){       
-   var scroll_start = 0;
+   //sticky navbar
+   var scroll_start = 0;  
    var startchange = $('#startchange');
    var offset = startchange.offset();
     if (startchange.length){
@@ -12,7 +13,7 @@ $(document).ready(function(){
        }
    });
     }
-
+  //smoth scroll
   // Select all links with hashes
 $('a[href*="#"]')
   // Remove links that don't actually link to anything
@@ -49,37 +50,17 @@ $('a[href*="#"]')
       }
     }
   });
-  /*
-$('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') 
-        || location.hostname == this.hostname) {
-
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-           if (target.length) {
-             $('html,body').animate({
-                 scrollTop: target.offset().top
-            }, 1000);
-            return false;
-        }
-    }
-  }); */
+  // fade in
   /* Every time the window is scrolled ... */
-    $(window).scroll( function(){
-    
+    $(window).scroll( function(){ 
         /* Check the location of each desired element */
-        $('.hideme').each( function(i){
-            
+        $('.hideme').each( function(i){        
             var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
+            var bottom_of_window = $(window).scrollTop() + $(window).height();          
             /* If the object is completely visible in the window, fade it it */
             if( bottom_of_window > bottom_of_object ){
-                
-                $(this).animate({'opacity':'1'},500);
-                    
-            }
-            
+                $(this).animate({'opacity':'1'},500);                 
+            }        
         });   
     });
 });
